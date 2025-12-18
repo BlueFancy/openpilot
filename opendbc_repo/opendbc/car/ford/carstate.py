7955b448012cddc26e37d77ab1ac88974d7f30f4
BlueFancy <49391022+BlueFancy@users.noreply.github.com>
2025-12-18 17:43:44 +0800

    Update carstate.py
    修改配置

--- opendbc_repo/opendbc/car/ford/carstate.py
+++ opendbc_repo/opendbc/car/ford/carstate.py
@@ -62,9 +62,8 @@ class CarState(CarStateBase, MadsCarState):
 
     if self.CP.flags & FordFlags.ALT_STEER_ANGLE:
       self.vehicle_sensors_valid = (
-        int((cp.vl["ParkAid_Data"]["ExtSteeringAngleReq2"] + 1000) * 10) not in (32766, 32767)
-        and cp.vl["ParkAid_Data"]["EPASExtAngleStatReq"] == 0
-        and cp.vl["ParkAid_Data"]["ApaSys_D_Stat"] in (0, 1)
+        int((cp.vl["BrakeSnData_5"]["SteWhlRelInit_An_Sns"] + 1600) * 10) not in (32766, 32767)
+
       )
     else:
    	  # Occasionally on startup, the ABS module recalibrates the steering pinion offset, so we need to block engagement
@@ -94,7 +93,7 @@ class CarState(CarStateBase, MadsCarState):
     if self.CP.flags & FordFlags.ALT_STEER_ANGLE:
       steering_angle_init = cp.vl["SteeringPinion_Data_Alt"]["StePinRelInit_An_Sns"]
       if self.vehicle_sensors_valid:
-        steering_angle_est = cp.vl["ParkAid_Data"]["ExtSteeringAngleReq2"]
+        steering_angle_est = cp.vl["BrakeSnData_5"]["SteWhlRelInit_An_Sns"]
         self.steering_angle_offset_deg = steering_angle_est - steering_angle_init
       ret.steeringAngleDeg = steering_angle_init + self.steering_angle_offset_deg
     else:
@@ -295,7 +294,7 @@ class CarState(CarStateBase, MadsCarState):
     if CP.flags & FordFlags.ALT_STEER_ANGLE:
       pt_messages += [
         ("SteeringPinion_Data_Alt", 100),
-        ("ParkAid_Data", 50),
+        ("BrakeSnData_5", 50),
         ("TransGearData",10),
       ]
     else:
