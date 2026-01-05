a298d8d8b20e5b8088c231974363327f87b7bb77
BlueFancy <49391022+BlueFancy@users.noreply.github.com>
2026-01-05 09:50:17 +0800

    Update carstate.py
    修改兼容

--- opendbc_repo/opendbc/car/ford/carstate.py
+++ opendbc_repo/opendbc/car/ford/carstate.py
@@ -59,11 +59,10 @@ class CarState(CarStateBase, MadsCarState):
     ret = structs.CarState()
     ret_sp = structs.CarStateSP()
 
+    # [4.0修改] 使用BrakeSnData_5而不是ParkAid_Data（适配您的车型）
     if self.CP.flags & FordFlags.ALT_STEER_ANGLE:
       self.vehicle_sensors_valid = (
-        int((cp.vl["ParkAid_Data"]["ExtSteeringAngleReq2"] + 1000) * 10) not in (32766, 32767)
-        and cp.vl["ParkAid_Data"]["EPASExtAngleStatReq"] == 0
-        and cp.vl["ParkAid_Data"]["ApaSys_D_Stat"] in (0, 1)
+        int((cp.vl["BrakeSnData_5"]["SteWhlRelInit_An_Sns"] + 1600) * 10) not in (32766, 32767)
       )
     else:
    	  # Occasionally on startup, the ABS module recalibrates the steering pinion offset, so we need to block engagement
@@ -89,10 +88,11 @@ class CarState(CarStateBase, MadsCarState):
     ret.parkingBrake = cp.vl["DesiredTorqBrk"]["PrkBrkStatus"] in (1, 2)
 
     # steering wheel
+    # [4.0修改] 使用BrakeSnData_5而不是ParkAid_Data（适配您的车型）
     if self.CP.flags & FordFlags.ALT_STEER_ANGLE:
       steering_angle_init = cp.vl["SteeringPinion_Data_Alt"]["StePinRelInit_An_Sns"]
       if self.vehicle_sensors_valid:
-        steering_angle_est = cp.vl["ParkAid_Data"]["ExtSteeringAngleReq2"]
+        steering_angle_est = cp.vl["BrakeSnData_5"]["SteWhlRelInit_An_Sns"]
         self.steering_angle_offset_deg = steering_angle_est - steering_angle_init
       ret.steeringAngleDeg = steering_angle_init + self.steering_angle_offset_deg
     else:
@@ -348,11 +348,12 @@ class CarState(CarStateBase, MadsCarState):
       print("Battery_Traction_4_FD1 signal exists (get_can_parser)")
       pt_messages.append(("Battery_Traction_4_FD1", 10))
 
+    # [4.0修改] 使用BrakeSnData_5而不是ParkAid_Data（适配您的车型）
     if CP.flags & FordFlags.ALT_STEER_ANGLE:
       pt_messages += [
         ("SteeringPinion_Data_Alt", 100),
-        ("ParkAid_Data", 50),
-        ("TransGearData",10),
+        ("BrakeSnData_5", 50),  # 4.0修改：使用BrakeSnData_5
+        ("TransGearData", 10),
       ]
     else:
       pt_messages += [
@@ -360,7 +361,7 @@ class CarState(CarStateBase, MadsCarState):
       ]
       if CP.transmissionType == TransmissionType.automatic:
         pt_messages += [
-          ("PowertrainData_10",10)
+          ("PowertrainData_10", 10)
         ]
 
     if CP.flags & FordFlags.CANFD:
@@ -412,3 +413,4 @@ class CarState(CarStateBase, MadsCarState):
       Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, CanBus(CP).main),
       Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.pt], cam_messages, CanBus(CP).camera),
     }
+
